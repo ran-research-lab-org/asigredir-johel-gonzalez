3dec910b61bbc420c2f33ce65d4cbbf4a991b0ad
johel-gonzalez <johel.gonzalez@upr.edu>
2024-10-11 02:30:36 +0000

    botherProfTomorrow

--- countAverage.cpp
+++ countAverage.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int main() {
+    
   int n, avgFull;
-  int ctr = 0;
   
   cout << "Entra 10 enteros: ";
 
   for (int i = 0; i < 10; i++) {
     cin >> n;
     avgFull = avgFull + n;
-    ctr++;
+    
   }
 
-  cout << format("Promedio de los numeros: {}\n", avgFull / 10);
+  cout << format("Promedio de los numeros: {}\n", avgFull / 10.);
   return 0;
 }
